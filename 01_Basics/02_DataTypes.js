2dee574e69dceca3fd1447d6d71256b8d354e98b
TheGraphicalMe <107987258+TheGraphicalMe@users.noreply.github.com>
2025-08-17 10:59:00 +0000

    datatypes done

---
+++ 01_Basics/02_DataTypes.js
@@ -0,0 +1,14 @@
+let age = 18
+let isLoggedIn = false
+let state = null
+
+/* number 
+bigint
+string
+boolean
+null => standalone value (type is given as object)
+undefined => undefined
+object*/
+console.log(typeof null)  // object
+console.log(typeof undefined) // undefined
+ 
--- 01_Basics/test.js
+++ 01_Basics/test.js
@@ -1,1 +1,7 @@
+let names = 'ayushsingh'
+const rno = '2452'
 console.log("Ayush")
+console.log(names)
+console.log(rno)
+names  = 'harkirat'
+console.log(names)
