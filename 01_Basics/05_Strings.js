56074b6930b731888f8470c42d11eb2af365d091
TheGraphicalMe <107987258+TheGraphicalMe@users.noreply.github.com>
2025-08-19 14:12:32 +0000

    strings

---
+++ 01_Basics/05_Strings.js
@@ -0,0 +1,11 @@
+let name = "ayush";
+let age = 20
+console.log(`hello my name is ${name} and my age is ${20}`);
+
+let gameName = "pubg"
+console.log (gameName.toUpperCase())
+console.log(gameName.charAt(2))
+
+let url = "https://laughing-space-pancake-x7rvjp55545c447.github.dev/"
+let split = url.split('-')
+console.log (split)
