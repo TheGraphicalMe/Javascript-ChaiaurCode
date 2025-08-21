d67899e39a6f2ff8ff286e43eeedf1220710e705
TheGraphicalMe <107987258+TheGraphicalMe@users.noreply.github.com>
2025-08-21 13:55:49 +0000

    number and maths done

---
+++ 01_Basics/06_nums_and_maths.js
@@ -0,0 +1,6 @@
+const balance = new Number(100.35976)
+console.log(balance.toString().length)
+console.log(balance.toFixed(2))
+
+const otherNumber = 23.9899
+console.log(otherNumber.toPrecision(3))
