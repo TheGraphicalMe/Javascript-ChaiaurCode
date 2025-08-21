f8cbef50022570fa18307583bdf377ca624b5f7a
TheGraphicalMe <107987258+TheGraphicalMe@users.noreply.github.com>
2025-08-21 14:12:41 +0000

    maths

--- 01_Basics/06_nums_and_maths.js
+++ 01_Basics/06_nums_and_maths.js
@@ -3,4 +3,14 @@ console.log(balance.toString().length)
 console.log(balance.toFixed(2))
 
 const otherNumber = 23.9899
-console.log(otherNumber.toPrecision(3))
+console.log(otherNumber.toPrecision(3)) 
+
+const hundreds = 1000000
+console.log(hundreds.toLocaleString('en-IN'))
+
+//**********************MATHS************************ */
+console.log(Math.abs(-4))
+console.log(Math.round(56.8));
+console.log(Math.floor(56.8));
+console.log(Math.random())  // 0-1 values
+console.log((Math.random()*10) + 1)  // values btw 1-9
