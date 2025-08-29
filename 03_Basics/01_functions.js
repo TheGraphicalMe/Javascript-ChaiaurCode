05a1e94655400079994b25fb05b5a0a58d9cd7d2
TheGraphicalMe <107987258+TheGraphicalMe@users.noreply.github.com>
2025-08-29 06:05:14 +0000

    funx1

---
+++ 03_Basics/01_functions.js
@@ -0,0 +1,15 @@
+function addTwoNumbers(number1 , number2){ //-->parameters
+    return number1 + number2
+} 
+
+const result = addTwoNumbers (3 , 4) // --> arguments
+console.log (result)
+
+
+function isLoggedIn(username){
+    return `${username} just logged in`
+}
+
+console.log(isLoggedIn()); //--> if no value is passed then it is printed as undefined
+
+
