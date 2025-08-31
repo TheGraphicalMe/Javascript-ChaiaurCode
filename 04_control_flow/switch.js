3ba81399906c4e997cf0ca651e1d9c6f77bed70f
TheGraphicalMe <107987258+TheGraphicalMe@users.noreply.github.com>
2025-08-31 13:31:54 +0000

    for loops

--- 04_control_flow/switch.js
+++ 04_control_flow/switch.js
@@ -15,3 +15,14 @@ switch (monthNumber) {
         console.log("other month");
         break;
 }
+
+let val1 = null ?? undefined ?? 10 //nullish coelscing operator
+console.log(val1);
+
+//ternary operator(condition ? true : false)
+
+const price = 800
+price > 900 ? console.log("greater than 900") : console.log("less than 900");
+
+
+
---
+++ 05_iterations/01_for_loops.js
@@ -0,0 +1,21 @@
+//for
+
+// for( let index = 0 ; index < 10 ; index++){
+//     console.log(index)
+//  }
+
+ //break n continue
+// for (let i = 0; i <= 10; i++) {
+//     if(i == 5){
+//         console.log("5 is detected");
+//         break;
+//     }
+//     console.log(i);
+//}
+for (let i = 0; i <= 10; i++) {
+    if(i == 5){
+        continue;
+    }
+    console.log(i);
+}
+
