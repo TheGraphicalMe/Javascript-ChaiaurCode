62e55f1b4bc7ac6f6b4531b2bca526e050e5581b
TheGraphicalMe <107987258+TheGraphicalMe@users.noreply.github.com>
2025-08-31 11:13:39 +0000

    if else

---
+++ 04_control_flow/one.js
@@ -0,0 +1,13 @@
+//if
+const isUserLoggedIn = true
+if (isUserLoggedIn){
+    
+}
+/// "==="  check the data type also
+
+if (2 === '2'){
+    console.log("same");
+}
+else{
+    console.log("diff data type");
+}
