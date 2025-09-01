a6103cb83fff9cd393214f0c7352b93811e69086
TheGraphicalMe <107987258+TheGraphicalMe@users.noreply.github.com>
2025-09-01 15:11:56 +0000

    for each done

---
+++ 05_iterations/03_for_of_and_for_in.js
@@ -0,0 +1,35 @@
+// for of 
+
+const arr = [1 , 2 , 3 , 4 , 5]
+for (const num of arr) {
+    // console.log(num);
+    
+}
+//Maps
+
+const map = new Map()  //--> for of loop for maps
+map.set('IN' , 'INDIA')
+map.set('USA' , 'UNITED STATES OF AMERICA')
+map.set('FR' , 'FRANCE')
+
+for (const [key,value] of map){
+    console.log(`${key} :- ${value}`)
+}
+// for of loop cant be used for objects
+
+const myObj = {
+    py: "python",
+    cpp: "C++",
+    rb: "ruby",
+    swift: "swift by apple"
+}
+
+for (const key in myObj){
+    console.log(`${key} shortcut is for ${myObj[key]}`);
+}
+//for in
+const heros = ["batman" , "spidermon" , "ironman"]
+for (const key in heros) {
+    //console.log(key)  --> for in return key or index 
+    console.log(heros[key])
+}
---
+++ 05_iterations/04_for_each.js
@@ -0,0 +1,40 @@
+// for Each
+
+const coding = ["js" , "ruby" , "java" , "python" , "cpp"]
+
+// coding.forEach ( function (item) {
+//     console.log(item)
+// })
+
+// coding.forEach( (val) => {
+//     console.log(val)
+// })
+
+// function printMe (x){
+//     console.log(x);
+// }
+
+// coding.forEach(printMe)
+
+coding.forEach((item , index , arr) => {
+    console.log(item , index , arr)
+})
+
+const myCoding = [
+    {
+        languageName: "javascript" , 
+        lanuguageFileName: "js"
+    },
+    {
+        languageName: "java" , 
+        lanuguageFileName: "java"
+    },
+    {
+        languageName: "python" , 
+        lanuguageFileName: "py"
+    }
+]
+
+myCoding.forEach( (item) => {
+    console.log(item.languageName);
+})
